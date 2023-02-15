627070227771351d87bebe4e822e6ee8304f0435
hidemaru-jp <111981295+hidemaru-jp@users.noreply.github.com>
2023-02-15 15:17:40 +0900

    [DELETE]not_necessary_CV

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -19,31 +19,7 @@ class Cart extends Model
         return $this->belongsTo('App\Models\Item', 'item_id');
     }
 
-    public function insert($item_id, $add_qty) {
-        $item = (new item)->findOrFail($item_id);
-        $qty = $item->quantity;
-        //在庫なしバリデーション
-        if ($qty <= 0) {
-            return false;
-        }
-        $cart = $this->firstOrCreate(['user_id' => Auth::id(), 'item_id' => $item_id], ['quantity' => 0]);
-        DB::beginTransaction();
-        try {
-            $cart->increment('quantity', $add_qty);
-            $item->decrement('quantity', $add_qty);
-            DB::commit();
-            return true;
-        } catch (Exception) {
-            DB::rollback();
-            return false;
-        }
-    }
     public function subtotal() {
-        $result = $this->item->price * $this->quantity;
-        return $result;
-    }
-
-    public function store() {
         $result = $this->item->price * $this->quantity;
         return $result;
     }
